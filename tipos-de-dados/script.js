ec0a9b8f14a2803d979ca64aefd220613b48389e
gridialves <gridialves@gmail.com>
2019-07-24 18:51:19 -0300

    primeiros exercicios

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,10 @@
+let qntd = 1;
+let frase = `A Orna lançou ${qntd *2} nova Tarumã `;
+console.log(frase);
+
+let idade = 21;
+let nome = 'Yngrid';
+    sobrenome = 'Alves';
+let nomeSobrenome = `Meu nome é ${nome +' '+ sobrenome} tenho ${idade} anos`;
+let frase2 =  "It's time";
+console.log(typeof nome); 
---
+++ variaveis/script.js
@@ -0,0 +1,14 @@
+let nome = 'Yngrid';
+let idade = 21;
+let comidaFav;
+
+comidaFav = 'Temaki';
+
+let profissao,
+    corFav,
+    musicaFav,
+    lugarFav,
+    bebidaFav;
+
+console.log(nome, idade, comidaFav);
+
