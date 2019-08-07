4f6fc1297cd2861a96343ab8ee73847e40b675e0
gridialves <gridialves@gmail.com>
2019-08-07 10:01:55 -0300

    numeros e operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,19 @@
+let total = 10 + 5 *  2 / 2 + 20;
+
+let naNString = 'eu sou a gridi'
+let  number = 5
+console.log(+naNString + number)
+
+let soma = '200'
+let numero = 50
+console.log(+soma + numero)
+
+let incremento = 5
+console.log(++incremento)
+
+let numero2 = +'80' / 2;
+let unidade = 'kg';
+let peso = numero2 + unidade
+console.log(peso)
+
+
